4a76e34e31842cdc292b67f1861ed26804c4f21a
saiteja <saitejaguntupalli296@gmail.com>
2024-01-20 22:38:24 +0530

    list Series

--- Series.py
+++ Series.py
@@ -6,7 +6,13 @@
 #  #python Int Data into pandas.Series
 # s = pd.Series(10)
 # print(s)
-import pandas as pd 
- #python Flot Data into pandas.Series
-s = pd.Series(10.0)
-print(s)
+# import pandas as pd 
+#  #python Flot Data into pandas.Series
+# s = pd.Series(10.0)
+# print(s)
+import pandas as pd
+import numpy as np
+lst = [int(x) for x in range(1,11)]
+p = np.array(lst)
+data = pd.Series(p,index=['A','B','C','D','E','F','G','H','I','J'])
+print(data)

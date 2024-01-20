ee53de281969da6a5aae5e9c1cf42a0327544abc
saiteja <saitejaguntupalli296@gmail.com>
2024-01-20 22:33:21 +0530

    float Data

--- Series.py
+++ Series.py
@@ -2,7 +2,11 @@
 # #python Str Data into pandas.Series
 # s = pd.Series('sai')
 # print(s)
+# import pandas as pd 
+#  #python Int Data into pandas.Series
+# s = pd.Series(10)
+# print(s)
 import pandas as pd 
- #python Int Data into pandas.Series
-s = pd.Series(10)
+ #python Flot Data into pandas.Series
+s = pd.Series(10.0)
 print(s)
